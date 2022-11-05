1a2f7ee85507b2bc1451016eb4fa09ba05276e05
Tim Brewis <timbrewis27@gmail.com>
2022-11-05 17:53:44 +0000

    feat: create basic CLI

--- compile_commands.py
+++ compile_commands.py
@@ -1,16 +1,61 @@
+import argparse
 import json
 import os
 import subprocess
+import sys
+
+def create_parser() -> argparse.ArgumentParser:
+    """Creates argument parser
+
+    Returns:
+        Argument parser
+    """
+    DESCRIPTION = 'Generates a compilation database from the output of make'
+    
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+
+    parser.add_argument('--compiler',
+                        '-c',
+                        metavar='COMPILER',
+                        type=str,
+                        required=True, # TODO: add compiler detection?
+                        help='compiler')
+
+    parser.add_argument('--dir',
+                        '-d',
+                        metavar='DIR',
+                        type=str,
+                        required=False,
+                        help='working directory to run `make` from',
+                        default=os.getcwd())
+
+    parser.add_argument('--extensions',
+                        '-e', 
+                        metavar='EXT', 
+                        type=str,
+                        nargs='+',
+                        required=True,
+                        help='file extension(s) for source files')
+
+    parser.add_argument('--output',
+                        '-o',
+                        metavar='FILE',
+                        required=False,
+                        help='output file',
+                        default='compile_commands.json')
+
+    return parser
+    
 
 if __name__ == '__main__':
 
-    COMPILER = 'gcc'
-    MAKE_DIR = './test'
-    OUTPUT_DIR = './test/build'
-    OUTPUT_FILE = 'compile_commands.json'
+    parser = create_parser()
+    args = parser.parse_args(sys.argv[1:])
+
+    print(args)
 
     # run make and capture the output
-    os.chdir(MAKE_DIR)
+    os.chdir(args.dir)
     
     subprocess.run(['make', 'clean'], 
                    stdout=subprocess.DEVNULL, 
@@ -27,12 +72,12 @@ if __name__ == '__main__':
 
     for line in make_output.splitlines():
         
-        if COMPILER not in line:
+        if args.compiler not in line:
             continue
 
         file = None
         for token in line.split(' '):
-            if any(end in token[-3:] for end in ['.c', '.s', '.S']):
+            if any(end in token[-3:] for end in args.extensions):
                 file = token.strip('\" ')
                 file = os.path.abspath(file)
                 break
@@ -41,13 +86,13 @@ if __name__ == '__main__':
             continue
 
         db_entry = {
-            "directory": MAKE_DIR,
+            "directory": args.dir, # TODO: this is not to spec
             "command": line,
-            "file": file
+            "file": file # TODO: this should just be the file name
         }
         
         compile_db.append(db_entry)
 
     # output result
-    with open(OUTPUT_FILE, 'w') as file:
+    with open(args.output, 'w') as file:
         file.write(json.dumps(compile_db, indent=4))
