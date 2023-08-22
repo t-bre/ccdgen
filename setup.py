c9910f3662bf153461ecbedbfacc339bdfe71761
Tim Brewis <timbrewis27@gmail.com>
2023-08-22 11:40:30 +0100

    chore: release v0.0.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setuptools.setup(
     name = "ccdgen",
-    version = "0.0.8",
+    version = "0.0.9",
     author = "Tim Brewis",
     author_email = "timbrewis27@gmail.com",
     license = "Apache-2.0",
